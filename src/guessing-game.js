188ec25473da9fa75162fc3fa2b316c810613f7f
Diana_Bobrova <dziana_babrova@epam.com>
2022-08-14 21:09:45 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        return this.guess();
     }
 
     guess() {
-
+        this.rand = Math.floor(((this.max + 1)  + this.min) / 2);
+        return this.rand;
     }
 
     lower() {
-
+        this.max = this.rand;
+        return this.max;
     }
 
     greater() {
+        this.min = this.rand;
+                return this.min;
 
     }
 }
 
 module.exports = GuessingGame;
+
+// const number = 598;
+// const game = new GuessingGame();
+// game.setRange(591, 606);
